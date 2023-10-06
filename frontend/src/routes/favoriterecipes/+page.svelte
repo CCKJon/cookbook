87f497578e5ee6bd15fed8ecf5b5da2a56a06069
Jonathan <Jonathan.Hwang.93@gmail.com>
2023-10-05 19:00:48 -0500

    favorites page list update

--- frontend/src/routes/favoriterecipes/+page.svelte
+++ frontend/src/routes/favoriterecipes/+page.svelte
@@ -1,3 +1,7 @@
 <script></script>
 
-<div>This is my favorite recipes page</div>
+<div
+	class="mx-auto w-11/12 py-7 px-7 h-full bg-[url('$lib/icons/kitchen.jpg')] overflow-hidden bg-no-repeat bg-cover text-gray-300"
+>
+	This is my favorite recipes page
+</div>
