b60b5b89b2cb34dd726955f30604e52d88fe9a2d
Jonathan <Jonathan.Hwang.93@gmail.com>
2023-10-17 21:08:50 -0500

    further card updates

--- frontend/src/routes/recipe-list/+page.svelte
+++ frontend/src/routes/recipe-list/+page.svelte
@@ -164,20 +164,31 @@
 					</a>
 				</div>
 			</div>
+			<div>
+				{#await getRecipeImage(imageId)}
+					<Spinner color="red" />
+				{:then imageUrl}
+					<img
+						class="w-auto max-h-[200px] object-cover"
+						src={URL.createObjectURL(imageUrl)}
+						alt=""
+					/>
+
+					<Card img="" class="mb-4">
+						<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+							{recipe.title}
+						</h5>
+						<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+							{recipe.description}
+						</p>
+						<a href={`/${recipe._id}`}>
+							<Button type="button">
+								Read more <ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-white" />
+							</Button>
+						</a>
+					</Card>
+				{/await}
+			</div>
 		{/each}
-		<div>
-			<Card img="/images/image-1.webp" reverse={vCard} class="mb-4">
-				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-					Noteworthy technology acquisitions 2021
-				</h5>
-				<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-					Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
-					chronological order.
-				</p>
-				<Button>
-					Read more <ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-white" />
-				</Button>
-			</Card>
-		</div>
 	</div>
 </div>
