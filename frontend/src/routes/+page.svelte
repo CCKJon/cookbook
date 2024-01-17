75a7506b629fc759767395e17850092ab82cfbe5
Jonathan <Jonathan.Hwang.93@gmail.com>
2024-01-16 21:05:39 -0600

    minor update

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	<div class="text-xs font-bold text-red-700 font-serif">WELCOME TO</div>
 	<div class="text-6xl text-gray-300 font-serif">Jonny's</div>
 	<div class="pb-1 text-6xl text-gray-300 font-serif">Cookbook</div>
-	<div class="pb-3 text-red-700 font-serif">A book containing the tastiest recipes</div>
+	<div class="pb-3 text-red-700 font-serif">A collection containing the tastiest recipes</div>
 	<button
 		type="button"
 		class="py-2border border-black w-28 h-8 text-center rounded-sm bg-yellow-500 text-gray-200 font-serif"
