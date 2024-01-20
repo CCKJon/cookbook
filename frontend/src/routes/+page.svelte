ab482642271bad7b05d9b67d690b82d8c3e340d0
Jonathan <Jonathan.Hwang.93@gmail.com>
2024-01-19 23:20:17 -0600

    minor updates

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <div
 	class="mx-auto w-11/12 py-7 px-7 h-full bg-[url('$lib/icons/kitchen.jpg')] overflow-hidden bg-no-repeat bg-cover"
 >
-	<div class="text-xs font-bold text-red-700 font-serif">WELCOME TO</div>
+	<div class="text-xs font-bold text-red-700 font-serif">WELCOME. TO.</div>
 	<div class="text-6xl text-gray-300 font-serif">Jonny's</div>
 	<div class="pb-1 text-6xl text-gray-300 font-serif">Cookbook!</div>
 	<div class="pb-3 text-red-700 font-serif">A collection containing the tastiest recipes</div>
